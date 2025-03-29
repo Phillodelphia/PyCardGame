c53409fe37b5e8876ba5ab93c97dabbc172d28ee
Phillodelphia <philip.vallentuna@gmail.com>
2025-03-29 14:37:23 +0100

    adding an example cardmeta

--- Cards/CardDB.py
+++ Cards/CardDB.py
@@ -1,4 +1,5 @@
 import pymongo
+from bson import json_util
 import os
 import json
 
@@ -11,6 +12,19 @@ def build_card_meta_database():
     collection = db["CardMeta"]
     for i in os.listdir():
         collection.insert_one(json.loads(i))
+    print("Card meta has successfully been built")
 
+# Function to create a new player
+def create_new_player(name):
+    player_meta = {
+        "name": name,
+        "player_inventory": [],
+    }
+    collection = db["PlayerDatabase"]
+
+    collection.insert_one(player_meta)
+    print("New player added to game")
+
+# Function to get all available cards
 def get_card_meta():
-    return db["CardMeta"]
+    return json_util.loads(db["CardMeta"])
